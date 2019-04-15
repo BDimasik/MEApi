a97723fd053e5741f1bab5bdf8d6c664a22f2285
MasterCapeXD <MasterCapeXD@users.noreply.github.com>
2019-04-15 19:02:09 +0300

    Refactoring

--- src/com/mineexperts/api/MineExperts.java
+++ src/com/mineexperts/api/MineExperts.java
@@ -1,8 +1,18 @@
 package com.mineexperts.api;
 
 import com.mineexperts.api.multiserver.ServersManager;
+import com.mineexperts.api.world.creatures.CreatureManager;
 
-public interface MineExperts {
-
-	ServersManager getServersManager();
+public final class MineExperts {
+	
+	private static ServersManager serversManager;
+	private static CreatureManager creatureManager;
+	
+	public static ServersManager getServersManager() {
+		return serversManager;
+	}
+	
+	public static CreatureManager getCreatureManager() {
+		return creatureManager;
+	}
 }
